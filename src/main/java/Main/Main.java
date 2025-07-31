d1449d41ff18392d8c3c80af16f922745d1044b8
rafi tanujaya <rafibussines@gmail.com>
2025-07-31 10:32:31 +0700

    feat(main) update scene javafx

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -4,7 +4,9 @@ import Config.DBConfig;
 import Controller.UserController;
 import Dao.UserDAO;
 import Service.UserService;
+import View.Login.LoginView;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.sql.Connection;
@@ -13,6 +15,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        LoginView loginView = new LoginView();
+
+        Scene scene = new Scene(loginView.getView(), 1220, 720);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("hallo");
+        primaryStage.show();
+
     }
 
     public static void main(String[] args) {
